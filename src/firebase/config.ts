f747161bc911a4f70ed9582ceedd45b9d4b9a136
Mert Mesut Demir <kolerak8@gmail.com>
2025-11-15 16:25:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2208157592-d48f1",
+  "appId": "1:1068253034432:web:61c8ff4748d637d1c1fa25",
+  "apiKey": "AIzaSyAgGqpuK1c1_WVlJtPumImpwXb4zfTDXBk",
+  "authDomain": "studio-2208157592-d48f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1068253034432"
+};
